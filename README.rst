a34abab259fbb7f39edd6b8cf04bfd5a396a3605
Mark Wolfman <canismarko@gmail.com>
2019-04-19 14:03:37 -0500

    Created basic project structure for travis, readthedocs, setup.py, etc.

---
+++ README.rst
@@ -0,0 +1,10 @@
+Franklin Citation Toolkit
+=========================
+
+.. image:: https://readthedocs.org/projects/franklin/badge/?version=latest
+  :target: https://franklin.readthedocs.io/en/latest/?badge=latest
+     :alt: Documentation Status
+.. image:: https://travis-ci.com/canismarko/franklin.svg?branch=master
+  :target: https://travis-ci.com/canismarko/franklin
+.. image:: https://coveralls.io/repos/github/canismarko/franklin/badge.svg?branch=master
+  :target: https://coveralls.io/github/canismarko/franklin?branch=master
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "franklin",
+    version = "0.1",
+    author = "Mark Wolfman",
+    author_email = "canismarko@gmail.com",
+    description = ("Retrieve scientific papers and manage associated citations.",),
+    license = "GPLv3",
+    keywords = "",
+    url = "https://github.com/canismarko/franklin",
+    packages=['franklin'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+        "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
+    ],
+)
